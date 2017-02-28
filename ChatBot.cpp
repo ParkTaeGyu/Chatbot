6769e39b66d21313aa7a8c59fe3192e776af9ef1
cmesias <healthypeanutts@gmail.com>
2017-02-27 16:56:18 -0800

    Add files via upload
    Added Chatbot.cpp

---
+++ ChatBot.cpp
@@ -0,0 +1,74 @@
+//============================================================================
+// Name        : ChatBot.cpp
+// Author      : cmesias
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
+
+int main() {
+
+	// Loop
+	bool quit = false;
+
+	// Welcome screen
+	cout << "Welcome to ChatBot V0.01" << "\n\n";
+
+	// Loop until user quits
+	while (!quit){
+
+		// Get user response
+		bool analyzeReponse = true;
+		cout << "YOU: ";
+		string response;
+		getline(cin, response);
+
+		// Quit if requested, else continue
+		if (response == "/quit"){
+			quit = true;
+			break;
+		}
+
+		// Compare response with saved responses
+		bool saveResponse = true;
+		string line;
+		ifstream brain("responses.txt");
+		if (brain.is_open()){
+			while (getline(brain, line)){
+				if (response == line){
+					getline(brain, line);
+					cout << "COMPUTER: " << line << "\n\n";
+					saveResponse = false;
+				}
+			}
+			brain.close();
+		}else{
+			cout << "No file present.";
+		}
+
+		// Response not in database, prompt user for correct response
+		if (saveResponse){
+			cout << "\nCOMPUTER: " << response << "\n";
+			cout << "YOU: ";
+			string response2;
+			getline(cin, response2);
+
+			ofstream out;
+			out.open("responses.txt", std::ios::app);
+			stringstream str;
+			str << "\n"
+				<< response  << "\n"
+				<< response2;
+			out << str.str().c_str();
+			cout << "\n";
+			out.close();
+		}
+	}
+
+	return 0;
+}
